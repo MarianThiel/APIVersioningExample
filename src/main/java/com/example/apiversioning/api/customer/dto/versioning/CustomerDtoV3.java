fc4e66ebfdab84a182db0a019fc2f67c75fa233b
marian.thiel <marian.thiel123@gmail.com>
2023-11-20 19:39:06 +0100

    removed security package, created nullptr constraint

--- src/main/java/com/example/apiversioning/api/customer/dto/versioning/CustomerDtoV3.java
+++ src/main/java/com/example/apiversioning/api/customer/dto/versioning/CustomerDtoV3.java
@@ -1,6 +1,7 @@
 package com.example.apiversioning.api.customer.dto.versioning;
 
 import com.example.apiversioning.api.common.versioning.CustomerDtoVersionable;
+import jakarta.annotation.Nonnull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -16,7 +17,7 @@ public class CustomerDtoV3 implements CustomerDtoVersionable {
 
     private String email;
 
-    private AddressDtoV2 address;
+    @Nonnull private AddressDtoV2 address;
 
     @Override
     public CustomerDtoVersionable convertUp() {
